df2e3b56540fde4bbe6fc2cc65a59be91da1a440
brennapacheco <brennapacheco@hotmail.com>
2022-03-22 22:49:54 -0300

    fix floating button and add confetti package

--- lib/pages/activitites_page.dart
+++ lib/pages/activitites_page.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:to_bem/components/patterns.dart';
 import '../components/activity_item.dart';
 import '../data/activity_data.dart';
+import 'package:confetti/confetti.dart';
 
 class ActivitiesPage extends StatefulWidget {
   const ActivitiesPage({Key? key}) : super(key: key);
@@ -13,16 +16,20 @@ class ActivitiesPage extends StatefulWidget {
 class _ActivitiesPageState extends State<ActivitiesPage> {
   int _selectedIndex = 0;
   late TextEditingController _feelingsTextController;
+  late ConfettiController _controllerBottomCenter;
 
   @override
   void initState() {
     super.initState();
     _feelingsTextController = TextEditingController();
+    _controllerBottomCenter =
+        ConfettiController(duration: const Duration(seconds: 5));
   }
 
   @override
   void dispose() {
     _feelingsTextController.dispose();
+    _controllerBottomCenter.dispose();
     super.dispose();
   }
 
@@ -35,11 +42,11 @@ class _ActivitiesPageState extends State<ActivitiesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF8F8FB),
       appBar: buildAppBar(),
       body: Column(
         children: [
           Container(
-            color: const Color(0xFFF8F8FB),
             margin: const EdgeInsets.only(left: 16, right: 16, top: 30),
             child: buildWelcomeText(),
           ),
@@ -58,25 +65,41 @@ class _ActivitiesPageState extends State<ActivitiesPage> {
               }).toList(),
             ),
             Container(
+                height: 100,
                 color: const Color(0xFFF8F8FB),
-                margin: const EdgeInsets.only(top: 490),
+                margin: const EdgeInsets.only(top: 430),
                 child: Column(
                   children: [
                     buildFeelingsInput(_feelingsTextController, context),
+                    Align(
+                      alignment: Alignment.center,
+                      child: ConfettiWidget(
+                        confettiController: _controllerBottomCenter,
+                        blastDirection: -pi / 2,
+                        emissionFrequency: 0.01,
+                        numberOfParticles: 20,
+                        maxBlastForce: 100,
+                        minBlastForce: 80,
+                        gravity: 0.3,
+                      ),
+                    ),
                   ],
                 )),
           ]),
         ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
       floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: const Color(0xFFAAC9CE),
+        backgroundColor: const Color(0xFFA6A9E8),
         label: const Text('Salvar'),
         icon: const Icon(
           Icons.done_rounded,
           color: Color(0xFFF8F8FB),
         ),
-        onPressed: () {}, //TODO: armazena atividades feitas + chama confetes
+        onPressed: () {
+          _controllerBottomCenter.play();
+        }, //TODO: armazena atividades feitas
       ),
       bottomNavigationBar: buildBottomNavBar(),
     );
